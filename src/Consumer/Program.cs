f1b74b4147c6a5ac4bf9d4e36dd2c8c3e6df8c2a
Matheus de Oliveira <mathnhais@outlook.com>
2022-05-17 22:11:14 -0300

    feat: add producer and consumer

--- src/Consumer/Program.cs
+++ src/Consumer/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Serilog;
+
+var logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+const string bootstrapServer = "localhost:9092";
+const string topicName = "my-topic";
+
+var config = new ConsumerConfig()
+{
+    GroupId = "demo-consumer-group",
+    BootstrapServers = bootstrapServer,
+    AutoOffsetReset = AutoOffsetReset.Latest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+
+consumer.Subscribe(topicName);
+
+var cts = new CancellationTokenSource();
+
+Console.CancelKeyPress += (_, e) =>
+{
+    logger.Warning("App shutting down");
+    
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+try
+{
+    logger.Information("Initializing consume of messages");
+    
+    while (true)
+    {
+        try
+        {
+            logger.Debug("Getting message");
+            
+            var consumeResult = consumer.Consume(cts.Token);
+            
+            logger.Information("Consumed message {@Result}", consumeResult);
+        }
+        catch (ConsumeException ex)
+        {
+            logger.Error("Error occured {ErrorReason}", ex.Error.Reason);
+        }
+    }
+    
+}
+catch (OperationCanceledException ex)
+{
+    consumer.Close();
+    logger.Warning("Operation canceled: {@Exception}", ex);
+}
--- src/Producer/Program.cs
+++ src/Producer/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Serilog;
+
+var logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+const string bootstrapServer = "localhost:9092";
+const string topicName = "my-topic";
+
+var config = new ProducerConfig()
+{
+    BootstrapServers = bootstrapServer,
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    logger.Information("Producer created");
+
+    for (int i = 0; i < 48; i++)
+    {
+        logger.Debug("Sending message");
+        
+        await producer.ProduceAsync(
+            topicName,
+            new Message<Null, string>()
+            {
+                Value = $"[{DateTime.Now.ToFileTime()}] Hello World from producer",
+            });   
+    }
+
+    logger.Information("Message sent to topic");
+}
+catch (ProduceException<Null, string> ex)
+{
+    logger.Error("Delivery failed: {@Exception}", ex);
+}
